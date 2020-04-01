469e514a223e3a3de9d070703c95b13bc0dd4133
lkurzweg <lin.kurzweg@gmail.com>
2020-04-01 12:05:02 +0200

    Include books component

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import books from '../mocks/books'
 import About from './About';
 import Footer from './Footer'
 import Navbar from './Navbar';
+import Books from './Books'
 
 class App extends React.Component {
     constructor(){
@@ -58,31 +59,7 @@ class App extends React.Component {
 
                 <Header title="ReactJS Academy"/>
 
-                <section id="books">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-lg-12 text-center">
-                                <h2>Books</h2>
-                                <hr className="star-primary"/>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className="col-lg-12">
-                                <ul className="nav nav-pills text-center">{tabItems}</ul>
-                            </div>
-                        </div>
-                        <div className="row book-list">
-                            {this.state.books.map(book=>(
-                                <div className="col-xs-6 col-sm-3" key={book.id}>
-                                    <div className="thumbnail">
-                                        <img alt="" className="img-responsive" src={book.cover}/>
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
-                    </div>
-                </section>
-
+                <Books books={this.state.books} tabItems={tabItems} />
                 <About/>
                 <Footer/>
             </div>
