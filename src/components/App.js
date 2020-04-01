0819bf77ccb55cea2f5f8ab1e6d850beeaa3c0a6
Karthikeyan Palaniswamy <karthikeyan.forethought@gmail.com>
2020-04-01 11:53:18 +0200

    Include Navbar component and pass toggleNav function as a props.

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Header from './Header'
 import books from '../mocks/books'
 import About from './About';
 import Footer from './Footer'
+import Navbar from './Navbar';
 
 class App extends React.Component {
     constructor(){
@@ -53,28 +54,7 @@ class App extends React.Component {
                     toggleMenu={this.toggleMenu}
                 />
 
-                <nav className="navbar navbar-default navbar-fixed-top navbar-custom">
-                    <div className="container">
-                        <div className="navbar-header">
-                            <a className="navbar-brand" href="/">
-                                ReactJS Academy
-                            </a>
-                        </div>
-                        <ul className="nav navbar-nav pull-right">
-                            <li className="hidden-xs">
-                                <a href="#about">About us</a>
-                            </li>
-                            <li>
-                                <button
-                                    onClick={this.toggleMenu}
-                                    className="btn btn-lg btn-outline"
-                                >
-                                    <i className="fa fa-graduation-cap"/> <span>Training</span>
-                                </button>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
+                <Navbar toggleMenu={this.toggleMenu}/>
 
                 <Header title="ReactJS Academy"/>
 
